65f3afc8c56b474ac0563a8f420bf7092d4a0532
Ryan Martz <martzrp@gmail.com>
2015-08-16 14:32:28 +0700

    stub out movement record view

---
+++ amatracker/MovementRecordTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovementRecordTableCell.swift
+//  amatracker
+//
+//  Created by ryan on 8/16/15.
+//  Copyright (c) 2015 ryan. All rights reserved.
+//
+
+import UIKit
+
+class MovementRecordTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
