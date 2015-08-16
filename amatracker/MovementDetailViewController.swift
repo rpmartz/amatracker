1561491d3f939139d8a2dd73274bed2949309886
Ryan Martz <martzrp@gmail.com>
2015-08-16 21:21:37 +0700

    load records from core data in movement list

--- amatracker/MovementDetailViewController.swift
+++ amatracker/MovementDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class MovementDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -14,12 +15,36 @@ class MovementDetailViewController: UIViewController, UITableViewDataSource, UIT
     @IBOutlet weak var movementRecordTableView: UITableView!
     
     var movement: Movement!
+    var records: [Record] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         movementNameLabel.text = movement.name
+        
+        // fetch the records
+        let request = NSFetchRequest(entityName: "Record")
+        
+        // add type since we're casting
+        let appDelegate : AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        let context = appDelegate.managedObjectContext!
+        
+        var error: NSError?
+        let fetchedRecords = context.executeFetchRequest(request, error: &error) as! [Record]
+        
+        var filteredRecords: [Record] = []
+        if fetchedRecords.count > 0 {
+            // TODO this should be done in the query
+            for record in fetchedRecords {
+                if(record.movement == movement) {
+                    filteredRecords.append(record)
+                }
+            }
+        }
+        
+        records = filteredRecords
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,15 +56,21 @@ class MovementDetailViewController: UIViewController, UITableViewDataSource, UIT
     // MARK: UITableViewDataSource functions
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
+        let record = records[indexPath.row]
         let cell = self.movementRecordTableView.dequeueReusableCellWithIdentifier("movementRecordCell") as! MovementRecordTableCell
-        cell.recordLabel.text = "Some Record"
+        cell.recordLabel.text = "\(record.numberOfReps) Reps"
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd-MMM-yyyy"
+        cell.dateLabel.text = dateFormatter.stringFromDate(record.date)
+        cell.weightLabel.text = "\(record.numberOfReps) kg"
         
         return cell
         
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return records.count
     }
     
     // MARK: UITableViewDelegate functions
--- amatracker/MovementRecordTableCell.swift
+++ amatracker/MovementRecordTableCell.swift
@@ -11,7 +11,8 @@ import UIKit
 class MovementRecordTableCell: UITableViewCell {
 
     @IBOutlet weak var recordLabel: UILabel!
-    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
