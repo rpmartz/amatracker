8d1eb6926e828990a0cdb6ad36a1f70fb873db3c
Ryan Martz <martzrp@gmail.com>
2015-10-24 10:06:55 +0700

    add calculator view

---
+++ amatracker/CalclatorViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var weightTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
