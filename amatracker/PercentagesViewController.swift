9a3ff0c3f97b242d66699b14595f52bd26c274b4
Ryan Martz <martzrp@gmail.com>
2015-10-24 11:19:57 +0700

    add table view controller

---
+++ amatracker/PercentagesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PercentagesViewController.swift
+//  amatracker
+//
+//  Created by ryan on 10/24/15.
+//  Copyright © 2015 ryan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PercentagesViewController : UITableViewController {
+    
+}
