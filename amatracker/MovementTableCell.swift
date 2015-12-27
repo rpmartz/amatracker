4f984155624829c8b40895b216f551c7eaf9ab6d
Ryan Martz <martzrp@gmail.com>
2015-12-27 07:16:41 -0500

    formatting

--- amatracker/MovementTableCell.swift
+++ amatracker/MovementTableCell.swift
@@ -5,13 +5,11 @@ class MovementTableCell : UITableViewCell {
     @IBOutlet weak var movementNameLabel: UILabel!
     
     override func awakeFromNib() {
-        // initialization code
         super.awakeFromNib()
     }
     
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
     
 }
