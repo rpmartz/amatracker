da6b3747ca12dc4a586f75615a977a414d1443ac
Ryan Martz <martzrp@gmail.com>
2016-01-23 16:42:50 -0500

    utility method to create some test data

--- amatracker/RecordService.swift
+++ amatracker/RecordService.swift
@@ -29,4 +29,60 @@ class RecordService {
             NSLog("\(error1)")
         }
     }
+    
+    func createTestRecords() -> Void {
+        let req = NSFetchRequest(entityName: "Movement")
+        
+        let context = appDelegate.managedObjectContext!
+        let movements = (try! context.executeFetchRequest(req)) as! [Movement]
+        
+        for movement in movements {
+            let entityDescription = NSEntityDescription.entityForName("Record", inManagedObjectContext: context)!
+            
+            // earlier but more than second 1RM
+            let firstOneRepMaxRecord = Record(entity: entityDescription, insertIntoManagedObjectContext: context)
+            firstOneRepMaxRecord.movement = movement
+            firstOneRepMaxRecord.numberOfReps = 1
+            firstOneRepMaxRecord.unit = "kg"
+            firstOneRepMaxRecord.weight = 100
+            firstOneRepMaxRecord.date = buildDate(2015, month: 10, day: 31)
+            
+            
+            let secondOneRM = Record(entity: entityDescription, insertIntoManagedObjectContext: context)
+            secondOneRM.movement = movement
+            secondOneRM.numberOfReps = 1
+            secondOneRM.unit = "kg"
+            secondOneRM.weight = 95
+            secondOneRM.date = NSDate()
+            
+            let threeReps = Record(entity: entityDescription, insertIntoManagedObjectContext: context)
+            threeReps.movement = movement
+            threeReps.numberOfReps = 3
+            threeReps.unit = "kg"
+            threeReps.weight = 70
+            threeReps.date = buildDate(2015, month: 11, day: 17)
+            
+            let fiveReps = Record(entity: entityDescription, insertIntoManagedObjectContext: context)
+            fiveReps.movement = movement
+            fiveReps.numberOfReps = 5
+            fiveReps.unit = "kg"
+            fiveReps.weight = 60
+            fiveReps.date = buildDate(2014, month: 07, day: 04)
+            
+        }
+        
+        appDelegate.saveContext()
+        
+        
+    }
+    
+    private func buildDate(year : Int, month : Int, day : Int) -> NSDate {
+        
+        let components = NSDateComponents()
+        components.year = year
+        components.month = month
+        components.day = day
+        
+        return NSCalendar.currentCalendar().dateFromComponents(components)!
+    }
 }
