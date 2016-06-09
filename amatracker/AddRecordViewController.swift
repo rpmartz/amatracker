b98f531c47810ae34b44996fd59ecd2f3c23402d
Ryan Martz <martzrp@gmail.com>
2016-06-09 18:19:19 -0400

    remove selector causing warning since it seemed to be unnecessary

--- amatracker/AddRecordViewController.swift
+++ amatracker/AddRecordViewController.swift
@@ -25,11 +25,10 @@ class AddRecordViewController: UIViewController {
         
         datePicker.datePickerMode = UIDatePickerMode.Date
         datePicker.setValue(UIColor.whiteColor(), forKey: "textColor")
-        datePicker.performSelector("setHighlightsToday:", withObject:UIColor.whiteColor())
         datePicker.maximumDate = NSDate()
         
         // when user touches outside keyboard, keyboard should go away
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(AddRecordViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
 
     }
