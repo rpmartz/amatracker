4a2bb672e29c792c4d79cb007534631c64f39def
Ryan Martz <martzrp@gmail.com>
2018-03-26 21:28:27 -0400

    fix weight label

--- amatracker/Movement Detail View Controller/View Models/MovementRecordViewModel.swift
+++ amatracker/Movement Detail View Controller/View Models/MovementRecordViewModel.swift
@@ -24,7 +24,7 @@ struct MovementRecordViewModel {
     }
     
     var weightDescription : String {
-        return "\(recordViewModel.weight) kg"
+        return "\(record.weight) kg"
     }
    
 }
